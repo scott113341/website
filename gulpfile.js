51c2265948909c98d9564de08ed57858e3891510
Scott Hardy <scott.the.hardy@gmail.com>
2014-06-05 02:03:20 -0700

    installed gulp-plumber, which makes gulp not crash on errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
 var uglify = require('gulp-uglify');
@@ -9,12 +10,14 @@ var nodemon = require('gulp-nodemon');
 
 gulp.task('html', function() {
   gulp.src(['./src/html/**/*.html'])
+    .pipe(plumber())
     .pipe(concat('index.html'))
     .pipe(gulp.dest('./public'));
 });
 
 gulp.task('css', function() {
   gulp.src(['./src/css/**/*.less'])
+    .pipe(plumber())
     .pipe(less())
     .pipe(concat('application.css'))
     .pipe(gulp.dest('./public'));
@@ -22,6 +25,7 @@ gulp.task('css', function() {
 
 gulp.task('js', function() {
   gulp.src(['./src/js/**/*.js'])
+    .pipe(plumber())
     .pipe(concat('application.js'))
     // .pipe(uglify())
     .pipe(gulp.dest('./public'));
