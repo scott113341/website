653577ad4ab5aeb6501684ed0edfa224be973cc7
Scott Hardy <scott.the.hardy@gmail.com>
2014-06-21 22:58:50 -0700

    updated project descriptions, public compiled

--- src/js/services/projectService.js
+++ src/js/services/projectService.js
@@ -4,24 +4,23 @@ app.service('projectService', function() {
       {
         name: 'SCU•classes',
         img: 'http://placehold.it/400x200',
-        link: 'http://scuclasses.com/',
+        link: 'http://scuclasses.com',
         src_link: 'https://github.com/scott113341/SCUclasses',
-        description: 'This is a description of the project.',
+        description: 'SCU•classes is a website I made for Santa Clara Students to streamline the process of registering for classes.  It launched in January 2013, and is now used by over 90% of the undergraduate population (about 5000 unique students each quarter).',
         technologies: ['Angular.js', 'Ruby on Rails', 'SASS', 'Bootstrap', 'PostgreSQL', 'Nokogiri', 'Rake']
       },
       {
         name: 'SCU•books',
         img: 'http://placehold.it/400x200',
-        link: 'http://scubooks.com/',
-        description: 'This is a description of the project.',
+        link: 'http://scubooks.com',
+        description: 'I\'ve long disliked how absurd college textbook prices are, so I built SCU•books for Santa Clara University students to find the lowest prices on their textbooks.  It pulls up the book list for your class schedule, then searches several online retailers for prices.',
         technologies: ['Angular.js', 'Ruby on Rails', 'API libraries', 'SASS', 'Bootstrap', 'Nokogiri', 'Rake']
       },
       {
         name: 'Powder Keg Brewing Company Website',
         img: 'http://placehold.it/400x200',
-        link: 'http://www.powderkegbrewingcompany.com/',
-        src_link: 'https://github.com/scott113341/SCUclasses',
-        description: 'This is a description of the project.',
+        link: 'http://www.powderkegbrewingcompany.com',
+        description: 'This is the website for Powder Keg Brewing Comapany, a new craft microbrewery in Niwot, Colorado.  The website is still under development (as is the brewery).',
         technologies: ['Angular.js', 'Ruby on Rails', 'SASS']
       }
     ];
