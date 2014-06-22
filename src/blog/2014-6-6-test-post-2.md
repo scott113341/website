72afa0b64da5492ee709f8e05894285a2ce4f4b8
Scott Hardy <scott.the.hardy@gmail.com>
2014-06-22 14:44:47 -0700

    wrote first blog post, closes #4

--- src/blog/2014-6-6-test-post-2.md
+++
@@ -1,4 +0,0 @@
-# Test Post 2
-#### June 6, 2014
-
-Here's some test blog post 2 written in [markdown](http://daringfireball.net/projects/markdown/)
--- src/js/services/blogPostService.js
+++ src/js/services/blogPostService.js
@@ -2,16 +2,10 @@ app.service('blogPostService', function() {
   this.posts = function() {
     return [
       {
-        title: 'Test Post 2',
-        path: '2014-6-6-test-post-2',
-        date: new Date(2014, 5, 6, 12, 50),
-        tldr: 'This is a short tl;dr of the blog post.'
-      },
-      {
-        title: 'Test Post',
-        path: '2014-6-5-test-post',
-        date: new Date(2014, 5, 5, 18, 24),
-        tldr: 'This is a short tl;dr of the blog post.'
+        title: 'First Post',
+        path: '2014-6-22-first-post',
+        date: new Date(2014, 6-1, 22),
+        tldr: 'My first blog post, mostly for testing purposes.  Nature and content of blog TBD.'
       }
     ];
   };
