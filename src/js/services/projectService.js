675ebff599f90a0d02ab3d71f0e61b0014e4159a
Scott Hardy <scott.the.hardy@gmail.com>
2014-06-21 21:14:51 -0700

    updated styling for projects, added more fields

--- src/js/services/projectService.js
+++ src/js/services/projectService.js
@@ -3,6 +3,7 @@ app.service('projectService', function() {
     return [
       {
         name: 'SCU•classes',
+        img: 'http://placehold.it/400x200',
         link: 'http://scuclasses.com/',
         src_link: 'https://github.com/scott113341/SCUclasses',
         description: 'This is a description of the project.',
@@ -10,13 +11,14 @@ app.service('projectService', function() {
       },
       {
         name: 'SCU•books',
+        img: 'http://placehold.it/400x200',
         link: 'http://scubooks.com/',
-        src_link: 'https://github.com/scott113341/SCUclasses',
         description: 'This is a description of the project.',
         technologies: ['Angular.js', 'Ruby on Rails', 'API libraries', 'SASS', 'Bootstrap', 'Nokogiri', 'Rake']
       },
       {
         name: 'Powder Keg Brewing Company Website',
+        img: 'http://placehold.it/400x200',
         link: 'http://www.powderkegbrewingcompany.com/',
         src_link: 'https://github.com/scott113341/SCUclasses',
         description: 'This is a description of the project.',
