0174193887110ca63cba4a7051576536718659b1
Scott Hardy <scott.the.hardy@gmail.com>
2014-06-23 12:05:20 -0700

    project images on s3

--- public/application.js
+++ public/application.js
@@ -22505,7 +22505,7 @@ app.service('projectService', function() {
     return [
       {
         name: 'SCU•classes',
-        img: 'http://placehold.it/400x200',
+        img: 'https://s3-us-west-2.amazonaws.com/scotthardy/scuclasses.png',
         link: 'http://scuclasses.com',
         src_link: 'https://github.com/scott113341/SCUclasses',
         description: 'SCU•classes is a website I made for Santa Clara Students to streamline the process of registering for classes.  It launched in January 2013, and is now used by over 90% of the undergraduate population (about 5000 unique students each quarter).',
@@ -22513,14 +22513,14 @@ app.service('projectService', function() {
       },
       {
         name: 'SCU•books',
-        img: 'http://placehold.it/400x200',
+        img: 'https://s3-us-west-2.amazonaws.com/scotthardy/scubooks.png',
         link: 'http://scubooks.com',
         description: 'I\'ve long disliked how absurd college textbook prices are, so I built SCU•books for Santa Clara University students to find the lowest prices on their textbooks.  It pulls up the book list for your class schedule, then searches several online retailers for prices.',
         technologies: ['Angular.js', 'Ruby on Rails', 'API libraries', 'SASS', 'Bootstrap', 'Nokogiri', 'Rake']
       },
       {
         name: 'Powder Keg Brewing Company Website',
-        img: 'http://placehold.it/400x200',
+        img: 'https://s3-us-west-2.amazonaws.com/scotthardy/pkbc.png',
         link: 'http://www.powderkegbrewingcompany.com',
         description: 'This is the website for Powder Keg Brewing Comapany, a new craft microbrewery in Niwot, Colorado.  The website is still under development (as is the brewery).',
         technologies: ['Angular.js', 'Ruby on Rails', 'SASS']
