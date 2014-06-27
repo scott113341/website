a1a2d0c24bffbdc91db3445e79313324bec7016a
Scott Hardy <scott.the.hardy@gmail.com>
2014-06-27 14:12:45 -0600

    moved express routes file to root

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-var routes = require('./routes/routes');
+var routes = require('./routes');
 
 app.use(express.static('./public'));
 app.use(routes);
