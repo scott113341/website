2c1cfeb74dd3865b3ed0e308a1626a6fda9fa759
Scott Hardy <scott.the.hardy@gmail.com>
2015-11-12 20:25:03 -0800

    formatting

--- src/js/services/BlogPostService.js
+++ src/js/services/BlogPostService.js
@@ -2,6 +2,12 @@ app.service('BlogPostService', function() {
   this.posts = function() {
     return [
       {
+        title: '3d Printed Custom Toilet Paper Holder',
+        path: '2015-11-12-toilet-paper-holder',
+        date: new Date(2015, 11-1, 12),
+        tldr: "Toilet paper holder was too small for Costco-sized rolls.  Designed and printed a custom TP holder that makes it work."
+      },
+      {
         title: 'The Great Car Experiment of 2015 is Over',
         path: '2015-10-19-the-great-car-experiment-of-2015-is-over',
         date: new Date(2015, 10-1, 19),
@@ -11,25 +17,25 @@ app.service('BlogPostService', function() {
         title: 'Living in my Car',
         path: '2015-8-30-living-in-my-car',
         date: new Date(2015, 8-1, 30),
-        tldr: 'Starting September 1st, I\'ll be living out of my car.  Random thoughts and rudimentary planning within.'
+        tldr: "Starting September 1st, I'll be living out of my car.  Random thoughts and rudimentary planning within."
       },
       {
         title: 'Big Sur Backpacking Trip',
         path: '2015-2-17-big-sur-backpacking',
         date: new Date(2015, 2-1, 17),
-        tldr: 'Trip log from a 4-day backpacking trip with Matt, Ryan, and Vanessa in the Big Sur area.'
+        tldr: "Trip log from a 4-day backpacking trip with Matt, Ryan, and Vanessa in the Big Sur area."
       },
       {
         title: 'The Colorado Trail',
         path: '2014-8-25-the-colorado-trail',
         date: new Date(2014, 8-1, 25),
-        tldr: 'I just finished an 8-day 104-mile backpacking trip from Denver to Breckenridge on The Colorado Trail (the whole thing is 500 miles to Durango).  This is my trip log.'
+        tldr: "I just finished an 8-day 104-mile backpacking trip from Denver to Breckenridge on The Colorado Trail (the whole thing is 500 miles to Durango).  This is my trip log."
       },
       {
         title: 'First Post',
         path: '2014-6-22-first-post',
         date: new Date(2014, 6-1, 22),
-        tldr: 'My first blog post, mostly for testing purposes.  Nature and content of blog TBD.'
+        tldr: "My first blog post, mostly for testing purposes.  Nature and content of blog TBD."
       }
     ];
   };
