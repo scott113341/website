01437562acb7d271613476a58917e631a7476989
Scott Hardy <scott.the.hardy@gmail.com>
2021-06-06 12:09:07 -0600

    More lints and updates

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,20 @@
 #!/usr/bin/env bash
 
-# all commands must exit 0
+# All commands must exit 0
 set -e
 
-# clean build folder
+# Clean build folder
 rm -rf build/
 
-# compile html
+# Compile html
 cp -r html/ build/
-pug --obj "{basedir: '$PWD'}" build/
+pug --obj "{ basedir: '$PWD' }" build/
 find build/ -type f -name *.pug -delete
 find build/ -type f -name *.jade -delete
 rm -r build/_templates/
 
-# compile css
+# Compile css
 lessc --clean-css css/app.less build/app.css
 
-# copy static files
+# Copy static files
 cp -r static/* build/
